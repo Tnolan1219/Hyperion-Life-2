a188596770b14c0edd15ea60a322f83e81c0fd3a
Thomas Nolan <tnolan1219@gmail.com>
2025-11-14 15:50:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4878656594-40849",
+  "appId": "1:333563405324:web:fa1d622df8e99177877a29",
+  "apiKey": "AIzaSyCNmsSPvmiKybGvLfDuDSpqcUNgKK1W1mc",
+  "authDomain": "studio-4878656594-40849.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "333563405324"
+};
